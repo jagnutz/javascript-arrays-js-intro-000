48577175abbea582db5cde39706cffbc15a82734
Michael <chaphlux@yahoo.com>
2018-12-26 15:18:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,51 +1,45 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var grilledCheeseIngredients = ['bread', 'mild cheese', 'sharp cheese', 'butter', 'tomato', 'garlic']
-var tomatoSauceIngredients = ['tomato', 'garlic', 'olive oil', 'basil', 'oregano']
-//to-do 1
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-//end of to-do
 function addElementToBeginningOfArray(array, element) {
-return [element, ...array]
+  return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-array.unshift(element)
-return array
-}
-
-function addElementToEndOfArray(array, element) {
-  return [array... , element]
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
-array.push(element)
-return array
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array
 }
 
 function accessElementInArray(array, index){
   return array[index]
 }
 
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
 function destructivelyRemoveElementFromBeginningOfArray(array){
-array.shift()
-return array
+  array.shift()
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
-array.pop()
-return array
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
 }
 
 function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }

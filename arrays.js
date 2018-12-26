513204b33d8af615841bdcb265488cfe12ec752f
Michael <chaphlux@yahoo.com>
2018-12-26 14:40:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,8 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift()
+array.shift()
+return array
 }
 
 function removeElementFromBeginningOfArray(array){
@@ -41,5 +42,6 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-return array.pop()
+array.pop()
+return array
 }

793114725e7f2290a51e76ed940538e42bb26b45
Michael <chaphlux@yahoo.com>
2018-12-26 14:25:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,8 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push('element')
 }
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
